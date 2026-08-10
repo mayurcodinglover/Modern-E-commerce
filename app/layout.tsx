7742ddb288f30bdda023d0b9a2391eefd2623923
Mayur <hedaumayur2003@gmail.com>
2026-08-10 23:02:45 +0530

    Build issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import ReduxProvider from "../components/providers/redux-provider";
 import { CartInitializer } from "../components/providers/cart-initializer";
 import {AuthInitializer} from "../components/providers/auth-initializer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <ReduxProvider>
           <AuthInitializer/>
            <CartInitializer />
